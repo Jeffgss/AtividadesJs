a5a8d967d1256e9c1aafebbad98f7cbc6ab9237a
Jefferson Gerson dos Santos <jefferson.santos@ENG-2589.maxmilhas.local>
2020-01-08 14:54:58 -0300

    Atividades Javascript

--- atividade2.js
+++ atividade2.js
@@ -1,10 +1,18 @@
 /*
 Escreva um programa que calcule e mostre a tabuada (até 10x) de um número qualquer, digitado pelo usuário.
 */
+const readline = require('readline')
 
-let num = window.prompt('Escolha um número para ver a tabuada');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-for (let i = 1; i < 10; i++) {
-    let multi = num * i;
-    console.log(`${num} x ${i} = ${multi}`);
-}
+rl.question('Me diz um numero ae malandro? ', (resp) => {
+    let num = Number(resp);
+    for (let i = 1; i < 11; i++) {
+        let multi = num * i;
+        console.log(`${num} x ${i} = ${multi}`);
+    }
+});
+
